8ff890d4f57e95b212f652ea7fa7c4efcb74f41f
whitemanthedj <awhiteman2000@gmail.com>
2022-04-13 22:42:19 -0400

    allow point clouds files to be analyzed for grab points

--- workspace/src/p_grab/scripts/computer_vision.py
+++ workspace/src/p_grab/scripts/computer_vision.py
@@ -7,8 +7,14 @@ from std_msgs.msg import String
 import octomap
 
 
-def pointcloud_to_octomap(pointcloud):
+def receive_pointcloud2_msg(octomap):
+    timestamp = None
+    if timestamp is None: # if the timestamp is not similar to that of the image we got from our service, do nothing
+        return
 
+    # find point cloud within octomap
+    # segment with openCV -- temporarily remove indices that are NOT applicable -- call this a query
+    # run each segments query on gpd_ros
     return
 
 
@@ -16,17 +22,26 @@ def find_grab_points(octomap):
 
     return
 
-
 def retrieve_pointcloud():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # run simultaneously.
     # name for our 'listener' node so that multiple listeners can
+
+    # listen on  service callback, only receive 1 image message
+    # wait for pointcloud to have similar timestamp
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, pointcloud_to_octomap)
+    rospy.Subscriber("chatter", String, receive_pointcloud2_msg)
 
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
+class ObjectGrabber:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    x = 2
