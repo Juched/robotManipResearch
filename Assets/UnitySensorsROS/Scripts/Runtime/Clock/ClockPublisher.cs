5f001ffa8bfed20d11782883ae8d11578cc75a15
Juched <prth.2000@gmail.com>
2022-04-21 02:41:07 -0400

    Changed camera params to be correct. Made stuff publish faster

--- Assets/UnitySensorsROS/Scripts/Runtime/Clock/ClockPublisher.cs
+++ Assets/UnitySensorsROS/Scripts/Runtime/Clock/ClockPublisher.cs
@@ -10,7 +10,9 @@ public class ClockPublisher : MonoBehaviour
 {
 
   [SerializeField] private string _topicName = "clock";
-    
+
+  private readonly static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
   private float _timeStamp   = 0f;
 
   private ROSConnection _ros;
@@ -30,11 +32,13 @@ public class ClockPublisher : MonoBehaviour
 
     void Update()
     {
-        this._timeStamp = Time.time;
-        uint sec = (uint)Math.Truncate(this._timeStamp);
-        uint nanosec = (uint)( (this._timeStamp - sec)*1e+9 );
-        this._message.clock.sec = sec;
-        this._message.clock.nanosec = nanosec;
+        TimeSpan timeSpan = DateTime.Now.ToUniversalTime() - UNIX_EPOCH;
+        double msecs = timeSpan.TotalMilliseconds;
+        uint secs = (uint)(msecs / 1000);
+        uint nsecs = (uint)((msecs / 1000 - secs) * 1e+9);
+        this._message.clock.sec = secs;
+        this._message.clock.nanosec = nsecs;
         this._ros.Send(this._topicName, this._message);
     }
+
 }
